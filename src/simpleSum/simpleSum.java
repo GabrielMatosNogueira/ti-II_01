848e9287483f59506d25f4a4f502b26ed709e814
GabrielMatosNogueira <gabrielmatosnogueiracompsci@gmail.com>
2025-09-18 18:15:49 -0300

    Exercicio01

---
+++ src/simpleSum/simpleSum.java
@@ -0,0 +1,19 @@
+package simpleSum;
+import java.util.*;
+
+public class simpleSum {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner (System.in);
+		int number01=0;
+		int number02=0;
+		
+		number01 = sc.nextInt();
+		number02 = sc.nextInt();
+		
+		System.out.println((number01 + number02));
+
+	}
+
+}
